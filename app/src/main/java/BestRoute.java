87f40c7fbf3fe0555f7a2123e81e99f53e979726
Lucas-Vial <viallucas03@gmail.com>
2025-06-25 16:34:23 +1000

    got started on BestRoute

--- app/src/main/java/BestRoute.java
+++ app/src/main/java/BestRoute.java
@@ -6,5 +6,50 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
 public class BestRoute {
-    //amamn
+    public static String cth = "2/38-70 Gartside St, Wanniassa, ACT, Australia";
+    public static String computeRoutes(String[] addresses){
+        try{
+            // API Endpoint
+            URL url = new URL("https://routes.googleapis.com/directions/v2:computeRoutes");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            // connection setup
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("X-Goog-Api-Key", "AIzaSyARl1eB1xlwwfu0bkG2sUiO7rRtB0Dh36o"); // Replace with your real API key
+            conn.setRequestProperty("X-Goog-FieldMask", "routes,geocodingResults.intermediates.intermediateWaypointRequestIndex");
+            conn.setDoOutput(true);
+
+            // Build the JSON request body
+            JSONObject requestBody = new JSONObject();
+            requestBody.put("origin", new JSONObject().put("address", cth ));
+            requestBody.put("destination", new JSONObject().put("address", cth));
+
+            // Add waypoints
+            JSONArray intermediates = new JSONArray();
+            for (String address : addresses) {
+                intermediates.put(new JSONObject().put("address", address));
+            }
+            requestBody.put("intermediates", intermediates);
+            requestBody.put("travelMode", "DRIVE");
+            requestBody.put("optimizeWaypointOrder", "true");
+
+            OutputStream os = conn.getOutputStream();
+            os.write(requestBody.toString().getBytes());
+            os.flush();
+            os.close();
+
+            Scanner scanner = new Scanner(conn.getInputStream());
+            StringBuilder response = new StringBuilder();
+            while (scanner.hasNext()) {
+                response.append(scanner.nextLine());
+            }
+            scanner.close();
+
+            return response.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
--- app/src/test/java/com/example/deliveryapp/TestCases.kt
+++ app/src/test/java/com/example/deliveryapp/TestCases.kt
@@ -5,14 +5,21 @@ import junit.framework.TestCase.assertTrue
 import org.junit.Test
 
 class TestCases {
+    val origin = "2/38-70 Gartside St, Wanniassa, ACT, Australia"
+    val dest = "33 Jackie Howe Crescent, Macarthur, ACT, Australia"
+    val dest2 = "65 Norriss St, Chisholm, ACT, Australia"
+    val dest3 = "3 Prichard Cct, Richardson, ACT, Australia"
 
     @Test
-    fun test(){
-        val origin = "33 Jackie Howe Crescent, Macarthur, ACT, Australia"
-        val dest = "2/38-70 Gartside St, Wanniassa, ACT, Australia"
-
+    fun testTesting(){
         val e = Testing.calcTime(origin, dest)
         print(e)
         assertTrue(e > 1)
+    }
+
+    @Test
+    fun bestRoute(){
+        val a = arrayOf(dest, dest2, dest3)
+        print(BestRoute.computeRoutes(a))
     }
 }
